d45443c65d6c30fee3afd59fcb85b3f660a4e27f
Wes Jonas <praebop@gmail.com>
2017-12-01 15:46:34 -0600

    Added additional files that were not versioned.

---
+++ Nozama/Controllers/OrdersController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Nozama.DAL;
+using Nozama.Models;
+
+namespace Nozama.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private NozamaContext db = new NozamaContext();
+
+        // GET: Checkout/Create
+        public ActionResult Index()
+        {
+            return View(db.Orders.ToList());
+        }
+    }
+}
---
+++ Nozama/Models/CartProducts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Nozama.Models
+{
+    public class CartProducts
+    {
+        [Key]
+        public Guid CartProductId { get; set; }
+
+        public virtual Product Product { get; set; }
+        public virtual Cart Cart { get; set; }
+    }
+}
